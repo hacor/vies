709c06ebabc820f26cf534721847731882002a1e
Jethro Van Thuyne <post@jethro.email>
2017-04-01 14:14:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const soap = require('soap');
+const endpoint = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
+
+module.exports = {
+    validate: Validate
+};
+
+function Validate(vat_number, country_code) {
+
+    const xmlRegex= /(<([^>]+)>)/ig;
+
+    const payload = {
+        vatNumber: vat_number,
+        countryCode: country_code.toUpperCase(),
+    };
+
+    return new Promise((resolve, reject) => {
+        soap.createClient(endpoint, (error, soapClient) => {
+            soapClient.checkVat(payload, (viesError, viesResult) => {
+                if (viesError) {
+                    return reject({
+                        error: viesError.body.split('<faultstring>')[1].replace(xmlRegex, ''),
+                        is_valid: false,
+                        data: null,
+                    });
+                }
+                resolve({
+                    error: null,
+                    is_valid: viesResult.valid,
+                    data: viesResult,
+                });
+            });
+        });
+    });
+
+}
